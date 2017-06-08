8d5713ede73aedb5ff61cc8859509dc05966e507
天空只能仰望吗 <351150710@qq.com>
2017-06-08 23:47:42 +0800

    简单调用WebService接口示例程序完成

---
+++ microservice-simple-provider-user/src/main/java/com/hysky/cloud/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.hysky.cloud.controller;
+
+import com.hysky.cloud.dao.UserRepository;
+import com.hysky.cloud.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id){
+        User user = this.userRepository.findOne(id);
+        return user;
+    }
+}
